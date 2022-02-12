3a24b81c5ada6428818fef8ed34763f15738ee72
Kevin De La Torre <kevindelatorre@live.com>
2022-02-12 14:27:03 -0600

    Add better_errors gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_31_023321) do
+ActiveRecord::Schema.define(version: 2022_02_12_200524) do
 
   create_table "animes", force: :cascade do |t|
     t.string "title"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2022_01_31_023321) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
