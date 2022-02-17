93ecff6de87c0a052c0e3cda35ef39b2a093b7d9
Kevin De La Torre <kevindelatorre@live.com>
2022-02-17 14:35:00 -0600

    Raise trigram search threshold to narrow searches

--- app/models/anime.rb
+++ app/models/anime.rb
@@ -1,6 +1,6 @@
 class Anime < ApplicationRecord
   include PgSearch::Model
-  pg_search_scope :search_title, against: :title, using: { tsearch: { prefix: true, dictionary: "english" }, trigram: { threshold: 0.1 } }
+  pg_search_scope :search_title, against: :title, using: { tsearch: { prefix: true, dictionary: "english" }, trigram: { threshold: 0.3 } }
   validates :title, uniqueness: true
   belongs_to :user
 end
